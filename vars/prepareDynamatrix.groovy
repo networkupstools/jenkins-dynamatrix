b58f1adab5ed83b76b50f65e93647c93d6e15fb4
Jim Klimov <jim@jimklimov.com>
2021-04-17 06:22:13 +0200

    prepareDynamatrix.groovy: PoC recursive resolveAxisValues() and resolveAxisNames()

--- vars/prepareDynamatrix.groovy
+++ vars/prepareDynamatrix.groovy
@@ -32,7 +32,7 @@ def parallelStages = prepareDynamatrix(
         ['-m32', '-m64'] ],
     dynamatrixRequiredLabelCombos: [[/OS=bsd/, /CLANG=12/]],
     mayFail: [[/OS=.+/, /GCCVER=4\..+/], ['ARCH=armv7l'], [/std.+=.+89/]],
-    skip: [[/OS=openindiana/, /CLANGVER=9/, /ARCH=x86/]]
+    skip: [[/OS=openindiana/, /CLANGVER=9/, /ARCH=x86/], [/GCCVER=4\.[0-7]\..+/, /std.+=+(!?89|98|99|03)/], [/GCCVER=4\.([8-9]|1[0-9]+)\..+/, /std.+=+(!?89|98|99|03|11)/]]
 ) {
     unstash 'preparedSource'
     sh """ ./autogen.sh """
@@ -47,5 +47,134 @@ def parallelStages = prepareDynamatrix(
 def call(Map<Object, Object> dynacfg, Closure body) {
     println "NOT IMPLEMENTED: prepareDynamatrix.groovy"
 
+    if (dynacfg.dynamatrixAxesLabels) {
+        println "No 'dynamatrixAxesLabels' were provided, nothing to generate"
+        return null
+    }
+
+    def nodeCaps = null
+    def buildLabels = []
+
+    if (dynacfg.commonLabelExpr) {
+        nodeCaps = infra.detectCapabilityLabelsForBuilders(dynacfg.commonLabelExpr)
+    } else {
+        nodeCaps = infra.detectCapabilityLabelsForBuilders()
+    }
+
+    Set effectiveAxes = []
+    for (axis in dynamatrixAxesLabels) {
+        effectiveAxes << resolveAxisName(dynacfg, nodeCaps, axis)
+    }
+    effectiveAxes = effectiveAxes.flatten()
+
+    println "[DEBUG] prepareDynamatrix(): Detected effectiveAxes: " + effectiveAxes
+
     return true;
+}
+
+def resolveAxisName(Map<Object, Object> dynacfg, Map<Object, Object> nodeCaps, Object axis) {
+    // The "axis" may be a fixed string to return quickly, or
+    // a regex to match among nodeCaps.nodeData[].labelMap[] keys
+    // or a groovy expansion to look up value(s) of another axis
+    // which is not directly used in the resulting build set.
+    // Recurse until (a flattened Set of) fixed strings with
+    // axis names (keys of labelMap[] above) can be returned.
+    Set res = []
+
+    // If caller has a Set to check, they should iterate it on their own
+    // TODO: or maybe provide a helper wrapper?..
+    if (axis == null || (!axis in [String, GString, java.util.regex.Pattern]) || axis.equals("")) {
+        println "[DEBUG] resolveAxisName(): invalid input value or class: " + axis.toString()
+        return res;
+    }
+
+    if (axis in String || axis in GString) {
+        def matcher = axis =~ /(\$\{.+\\})/
+        if (matcher.find()) {
+            // Substitute values of one expansion and recurse -
+            // if there are more dollar-braces, they will be
+            // expanded in the nested layer(s)
+            def varAxis = matcher[0][1]
+
+            // This layer of recursion gets us fixed-string name
+            // of the variable axis itself (like fixed 'COMPILER'
+            // string for variable part '${COMPILER}' in originally
+            // requested axis name '${COMPILER}VAR').
+            // ASSUMPTION: It might drill deeper for nested request
+            // like '${COMPILER${VENDOR}}VER' - recursion not checked
+            for (expandedAxisName in resolveAxisName(dynacfg, nodeCaps, varAxis)) {
+                if (expandedAxisName == null || (!expandedAxisName in String && !expandedAxisName in GString) || expAxis.equals("")) continue;
+
+                // This layer of recursion gets us fixed-string name
+                // variants of the variable axis (like 'GCC' and
+                // 'CLANG' for variable '${COMPILER}' in originally
+                // requested axis name '${COMPILER}VAR').
+                // Pattern looks into nodeCaps.
+                for (expandedAxisValue in resolveAxisValues(dynacfg, nodeCaps, expandedAxisName)) {
+                    if (expandedAxisValue == null || (!expandedAxisValue in String && !expandedAxisValue in GString) || expAxis.equals("")) continue;
+
+                    // In the original axis like '${COMPILER}VER' apply current item
+                    // from expandedAxisValue like 'GCC' (or 'CLANG' in next loop)
+                    // and yield 'GCCVER' as the axis name for original request:
+                    String tmpAxis = axis.replaceFirst(/\$\{${varAxis}\}/, expandedAxisValue)
+
+                    // Now resolve the value of "axis" with one substituted
+                    // expansion variant - if it is a fixed string by now,
+                    // this will end quickly:
+                    res << resolveAxisName(dynacfg, nodeCaps, tmpAxis)
+                }
+            }
+            return res.flatten()
+        } else {
+            res = [axis]
+            return res
+        }
+    }
+
+    if (axis in java.util.regex.Pattern) {
+        // Return label keys which match the expression
+        for (hudson.model.Node node : nodeCaps.nodeData.keySet) {
+            if (node == null) continue
+
+            for (String label : nodeCaps.nodeData[node].labelMap.keySet()) {
+                if (axis in java.util.regex.Pattern) {
+                    if (label =~ axis) {
+                        res << label
+                    }
+                }
+            }
+        }
+    }
+
+    return res.flatten()
+}
+
+def resolveAxisValues(Map<Object, Object> dynacfg, Map<Object, Object> nodeCaps, Object axis) {
+    // For a fixed-string name or regex pattern, return a flattened Set of
+    // values which have it as a key in nodeCaps.nodeData[].labelMap[]
+
+    Set res = []
+    if (axis == null || (!axis in [String, GString, java.util.regex.Pattern]) || axis.equals("")) {
+        println "[DEBUG] resolveAxisValues(): invalid input value or class: " + axis.toString()
+        return res;
+    }
+
+    for (hudson.model.Node node : nodeCaps.nodeData.keySet) {
+        if (node == null) continue
+
+        for (String label : nodeCaps.nodeData[node].labelMap.keySet()) {
+            if (axis in [String, GString]) {
+                if (axis.equals(label)) {
+                    res << nodeCaps.nodeData[node].labelMap[label]
+                }
+            }
+            if (axis in java.util.regex.Pattern) {
+                if (label =~ axis) {
+                    res << nodeCaps.nodeData[node].labelMap[label]
+                }
+            }
+        }
+    }
+
+    return res.flatten()
 }
