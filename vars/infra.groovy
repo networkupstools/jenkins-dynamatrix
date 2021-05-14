c4ca0cf67ad24c3984d847c594457b26265fbda1
Jim Klimov <jim@jimklimov.com>
2021-05-15 00:36:14 +0200

    infra.groovy: introduce withEnvOptional() variants

--- vars/infra.groovy
+++ vars/infra.groovy
@@ -1,4 +1,5 @@
 import org.nut.dynamatrix.dynamatrixGlobalState;
+import org.nut.dynamatrix.NodeData;
 import org.nut.dynamatrix.Utils;
 
 /*
@@ -29,5 +30,93 @@ def labelDocumentationWorker() {
         return dynamatrixGlobalState.labelDocumentationWorker
     }
     return labelDefaultWorker()
+}
+
+/* Helpers for optional use of withEnv - if a value is not already in the env
+ * map, use one defined by the current build agent, or the provided default.
+ */
+def withEnvOptional(String VAR, String DEFVAL, Closure body) {
+    if (env.containsKey(VAR)) {
+        return body()
+    }
+
+    def hits = 0
+    def VAL = null
+    if (Utils.isStringNotEmpty(env['NODE_NAME'])) {
+        def node = NodeData.getNodeByName(env.NODE_NAME)
+        if (node != null) {
+            for (label in node.labelString.split('[ \r\n\t]+')) {
+                if (label.startsWith("${VAR}=")) {
+                    String[] keyValue = label.split("=", 2)
+                    if (VAL == null) VAL=keyValue[1]
+                    hits ++
+                }
+            }
+        }
+
+        if (hits > 0 && VAL != null) {
+            if (hits > 1) println "[WARNING] Numerous assignments of label '${VAR}' in node '${env.NODE_NAME}': using envvar value '${VAL}'"
+            //return withEnv(["${VAR}": "${VAL}"], body)
+            return withEnv(["${VAR}=${VAL}"], body)
+        }
+    }
+
+    if (DEFVAL != null) {
+        //return withEnv(["${VAR}": "${DEFVAL}"], body)
+        return withEnv(["${VAR}=${DEFVAL}"], body)
+    }
+
+    return body()
+}
+
+def withEnvOptional(Map VARVAL, Closure body) {
+    if (VARVAL.size() == 0) {
+        return body()
+    }
+
+    def envmap = [:]
+    def node = null
+    if (Utils.isStringNotEmpty(env['NODE_NAME'])) {
+        node = NodeData.getNodeByName(env.NODE_NAME)
+    }
+
+    for (VAR in VARVAL.keySet()) {
+        def DEFVAL = VARVAL[VAR]
+        if (Utils.isStringNotEmpty(env[VAR]) || envmap.containsKey(VAR)) {
+            continue
+        }
+
+        def hits = 0
+        def VAL = null
+        if (node != null) {
+            for (label in node.labelString.split('[ \r\n\t]+')) {
+                if (label.startsWith("${VAR}=")) {
+                    String[] keyValue = label.split("=", 2)
+                    if (VAL == null) VAL=keyValue[1]
+                    hits ++
+                }
+            }
+
+            if (hits > 0 && VAL != null) {
+                if (hits > 1 && dynamatrixGlobalState.enableDebugTrace) println "[WARNING] Numerous assignments of label '${VAR}' in node '${env.NODE_NAME}': using envvar value '${VAL}'"
+                envmap[VAR] = "${VAL}"
+            }
+        }
+
+        if (DEFVAL != null) {
+            envmap[VAR] = "${DEFVAL}"
+        }
+    }
+
+    if (envmap.size() > 0) {
+        def envarr = []
+        for (k in envmap.keySet()) {
+            envarr << "${k}=${envmap[k]}"
+        }
+        //return withEnv(envmap, body)
+        return withEnv(envarr, body)
+    }
+
+    return body()
 }
 
