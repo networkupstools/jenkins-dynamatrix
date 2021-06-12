fbaf152adca0fac31761f386faeab3146e7aac1f
Jim Klimov <jim@jimklimov.com>
2021-06-13 00:28:11 +0200

    autotools.groovy: define configureEnvvars block to match our {COMPILER}VER settings

--- vars/autotools.groovy
+++ vars/autotools.groovy
@@ -35,8 +35,103 @@ def sanityCheckDynacfgPipeline(dynacfgPipeline = [:]) {
         }
 
         if (!dynacfgPipeline.containsKey('configure')) {
-            dynacfgPipeline['configure'] = "( [ -x configure ] || exit ; ./configure \${CONFIG_OPTS} )"
+            dynacfgPipeline['configure'] = " ( [ -x configure ] || exit; ./configure \${CONFIG_OPTS} ) "
         }
+
+        if (!dynacfgPipeline.containsKey('configureEnvvars')) {
+            dynacfgPipeline['configureEnvvars'] = """ {
+case "\${CONFIG_OPTS}" in
+    *" CC="*) ;;
+    CC=*) ;;
+    *)  if [ -n "\${CLANGVER}" ]; then
+            CONFIG_OPTS="\${CONFIG_OPTS} CC=clang-\${CLANGVER}"
+        else if [ -n "\${GCCVER}" ]; then
+            CONFIG_OPTS="\${CONFIG_OPTS} CC=gcc-\${GCCVER}"
+        fi; fi
+        ;;
+esac
+
+case "\${CONFIG_OPTS}" in
+    *" CXX="*) ;;
+    CXX=*) ;;
+    *)  if [ -n "\${CLANGVER}" ]; then
+            CONFIG_OPTS="\${CONFIG_OPTS} CXX=clang++-\${CLANGVER}"
+        else if [ -n "\${GCCVER}" ]; then
+            CONFIG_OPTS="\${CONFIG_OPTS} CXX=g++-\${GCCVER}"
+        fi; fi
+        ;;
+esac
+
+case "\${CONFIG_OPTS}" in
+    *" CPP="*) ;;
+    CPP=*) ;;
+    *)  if [ -n "\${CLANGVER}" ]; then
+            if command -v "clang-cpp-\${CLANGVER}" >/dev/null ; then
+                CONFIG_OPTS="\${CONFIG_OPTS} CPP=clang-cpp-\${CLANGVER}"
+            else
+                if command -v "clang-cpp" >/dev/null ; then
+                    CONFIG_OPTS="\${CONFIG_OPTS} CPP=clang-cpp"
+                fi
+            fi
+        else if [ -n "\${GCCVER}" ]; then
+            if command -v "cpp-\${GCCVER}" >/dev/null ; then
+                CONFIG_OPTS="\${CONFIG_OPTS} CPP=cpp-\${GCCVER}"
+            fi
+        fi; fi
+        ;;
+esac
+
+# May be a loophole here if user already passed a CFLAGS value,
+# just without a standard revision spec... Or with just one of those.
+STDARG=""
+STDXXARG=""
+case "\${CONFIG_OPTS}" in
+    *" -std="*) ;;
+    "-std="*) ;;
+    *)  # Adjust versions used for plain C (noop if only -std=c++... is passed by user)
+        if [ -n "\${CSTDVERSION_c}" ]; then
+            if [ "\${CSTDVERSION_c}" = ansi ]; then
+                STDARG="-std=ansi"
+            else
+                if [ -n "\${CSTDVARIANT}" ]; then
+                    STDARG="-std=\${CSTDVARIANT}\${CSTDVERSION_c}"
+                else
+                    STDARG="-std=c\${CSTDVERSION_c}"
+                fi
+            fi
+        fi
+        ;;
+esac
+
+case "\${CONFIG_OPTS}" in
+    *" -std="*++*) ;;
+    "-std="*++*) ;;
+    *)  # Adjust versions used
+        if [ -n "\${CSTDVERSION_cxx}" ]; then
+            if [ "\${CSTDVERSION_cxx}" = ansi ]; then
+                STDXXARG="-std=ansi"
+            else
+                if [ -n "\${CSTDVARIANT}" ]; then
+                    STDXXARG="-std=\${CSTDVARIANT}++\${CSTDVERSION_cxx}"
+                else
+                    STDXXARG="-std=c++\${CSTDVERSION_cxx}"
+                fi
+            fi
+        fi
+        ;;
+esac
+
+if [ -n "\${STDARG}" ]; then
+    CONFIG_OPTS="\${CONFIG_OPTS} CFLAGS=\${STDARG}"
+fi
+
+if [ -n "\${STDXXARG}" ]; then
+    CONFIG_OPTS="\${CONFIG_OPTS} CXXFLAGS=\${STDXXARG}"
+fi
+
+export CONFIG_OPTS
+} """
+        } // configureEnvvars
 
         if (!dynacfgPipeline.containsKey('build')) {
             dynacfgPipeline['build'] = "( \${MAKE} all )"
