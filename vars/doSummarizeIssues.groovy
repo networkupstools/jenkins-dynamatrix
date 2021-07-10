ae8d6cb7fbcda3a00c4db48fe7566f0521598594
Jim Klimov <jim@jimklimov.com>
2021-07-10 22:51:03 +0200

    doSummarizeIssues.groovy: better handle TARGET_BRANCH whose existence is not guaranteed

--- vars/doSummarizeIssues.groovy
+++ vars/doSummarizeIssues.groovy
@@ -12,5 +12,15 @@ void call(String sJOB_NAME, String sBRANCH_NAME, String sTARGET_BRANCH = "master
 } // doSummarizeIssues()
 
 void call() {
-    doSummarizeIssues("${JOB_NAME}", "${BRANCH_NAME}", TARGET_BRANCH ? "${TARGET_BRANCH}" : "master")
+    def sTARGET_BRANCH = "master"
+    try {
+        // Can fail if not set by pipeline (not a PR build)
+        sTARGET_BRANCH = "${TARGET_BRANCH}"
+    } catch (Throwable t1) {
+        try {
+            sTARGET_BRANCH = "${env.TARGET_BRANCH}"
+        } catch (Throwable t2) {}
+    }
+
+    doSummarizeIssues("${JOB_NAME}", "${BRANCH_NAME}", sTARGET_BRANCH)
 }
