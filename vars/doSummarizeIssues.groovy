b7c8cdc3ff067c3a0c04fe0eba99433637282337
Jim Klimov <jim@jimklimov.com>
2021-07-17 00:02:21 +0200

    doSummarizeIssues.groovy: modernize referenceJob(Name) specification=>discovery

--- vars/doSummarizeIssues.groovy
+++ vars/doSummarizeIssues.groovy
@@ -4,12 +4,15 @@ import org.nut.dynamatrix.*;
 void call(def issueAnalysisArr, String id, String name, String sJOB_NAME, String sBRANCH_NAME, String sTARGET_BRANCH) {
     if (issueAnalysisArr.size() > 0) {
         // Compare issues that are new/fixed compared to specified branch
-        // TODO: Update according to https://github.com/jenkinsci/warnings-ng-plugin/blob/master/doc/Documentation.md#configure-the-selection-of-the-reference-build-baseline
+        // ReferenceJob(Name) is handled according to current version (at this time) of
+        //   https://github.com/jenkinsci/warnings-ng-plugin/blob/master/doc/Documentation.md#configure-the-selection-of-the-reference-build-baseline
+        // Note: this solution below assumes Git SCM for the project
         def reference = sJOB_NAME.replace(sBRANCH_NAME, sTARGET_BRANCH)
+        discoverGitReferenceBuild referenceJob: reference
         publishIssues (
             id: id,
             name: name,
-            referenceJobName: reference,
+            //referenceJobName: reference,
             //recordIssues-only//filters: [includePackage('io.jenkins.plugins.analysis.*')],
             issues: issueAnalysisArr
             )
