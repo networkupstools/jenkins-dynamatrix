bc53c7fe04c6fcf27b29e90824d0ef3d2a92a738
Jim Klimov <jim@jimklimov.com>
2021-09-16 17:32:51 +0200

    Update stashCleanSrc.groovy

--- vars/stashCleanSrc.groovy
+++ vars/stashCleanSrc.groovy
@@ -1,6 +1,6 @@
 import org.nut.dynamatrix.DynamatrixStash
 
-void call(String stashName, Closure scmbody = null) {
+def call(String stashName, Closure scmbody = null) {
     // Optional closure can fully detail how the code is checked out
-    DynamatrixStash.stashCleanSrc(this, stashName, scmbody)
+    return DynamatrixStash.stashCleanSrc(this, stashName, scmbody)
 } // stashCleanSrc()
