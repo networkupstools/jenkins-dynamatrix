e00e2a360d7e9499f1bb70a5965e63fcc11a4cce
Jim Klimov <jim@jimklimov.com>
2021-10-03 22:21:56 +0200

    vars/sh.groovy: document more

--- vars/sh.groovy
+++ vars/sh.groovy
@@ -1,8 +1,19 @@
 /* For build agents that define a shell wrapper, such as ssh or chroot
  * into the "real" build environment, pass the "sh" argument command
  * through that, to be evaluated on that side.
+ *
  * The build agent entry on Jenkins should define a CI_WRAP_SH envvar like:
  *   `ssh -o SendEnv '*' jenkins-debian11-ppc64el /bin/sh `
+ * or
+ *   `/bin/sudo /usr/sbin/chroot /srv/libvirt/mycontainer/rootfs /bin/sh `
+ *
+ * Note that in case of SSH, the server in container should `AcceptEnv *`
+ * and permit non-interactive SSH login for the CI account (and have its
+ * working area in sync - the agent.jar on host interacts with filesystem);
+ * and in case of chroot, the CI account on host should be allowed to sudo
+ * the command (and bind-mounts used to pass the homedir).
+ *
+ * See NUT::docs/ci-farm-lxc-setup.txt for setup details.
  */
 def sh(shargs) {
     if (env?.CI_WRAP_SH) {
