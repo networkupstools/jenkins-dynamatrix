c7c16ca826bfb340494373a8b75ae425c908e2b1
Jim Klimov <jimklimov+nut@gmail.com>
2023-10-23 11:15:05 +0200

    Dynamatrix.prepareDynamatrix(): prefix logs with the URLs

--- src/org/nut/dynamatrix/Dynamatrix.groovy
+++ src/org/nut/dynamatrix/Dynamatrix.groovy
@@ -2632,6 +2632,7 @@ def parallelStages = prepareDynamatrix(
 
                 parstageCode = {
                     def parstageCompleted = false
+                    String buildArtifactUrlPrefixSlashed = "${env.BUILD_URL?.replaceLast('/', '')}/artifact/"
                     while (!parstageCompleted) {
                         try {
                             if (enableDebugSysprint)
@@ -2670,7 +2671,7 @@ def parallelStages = prepareDynamatrix(
                                         String msg = "'slow build' stage for ${MATRIX_TAG} did not pass: ${dsbc.dsbcResultInterim}"
                                         script.infra.reportGithubStageStatus(dynacfgOrig.get("stashnameSrc"),  // TODO: Fix into DSBC copy of dynacfg for mixed-config jobs?
                                                 msg, 'FAILURE', "slowbuild-run/${MATRIX_TAG}",
-                                                dsbc.getLatestDsbcResultLog())
+                                                buildArtifactUrlPrefixSlashed + dsbc.getLatestDsbcResultLog())
                                         parstageCompleted = true
                                     }
                                     break
@@ -2722,7 +2723,7 @@ def parallelStages = prepareDynamatrix(
                                         script.infra.reportGithubStageStatus(dynacfgOrig.get("stashnameSrc"),  // TODO: Fix into DSBC copy of dynacfg for mixed-config jobs?
                                                 "'slow build' stage for ${MATRIX_TAG} did not pass: ${dsbc.dsbcResultInterim}",
                                                 'FAILURE', "slowbuild-run/${MATRIX_TAG}",
-                                                dsbc.getLatestDsbcResultLog())
+                                                buildArtifactUrlPrefixSlashed + dsbc.getLatestDsbcResultLog())
                                     }
 
                                     parstageCompleted = true
@@ -2745,7 +2746,7 @@ def parallelStages = prepareDynamatrix(
                                                 "'slow build' stage for ${MATRIX_TAG} finished somehow " +
                                                 "with unexpected verdict: ${dsbc.dsbcResultInterim}",
                                                 'FAILURE', "slowbuild-run/${MATRIX_TAG}",
-                                                dsbc.getLatestDsbcResultLog())
+                                                buildArtifactUrlPrefixSlashed + dsbc.getLatestDsbcResultLog())
                                     }
 
                                     parstageCompleted = true
@@ -2762,7 +2763,7 @@ def parallelStages = prepareDynamatrix(
                                             "'slow build' stage for ${MATRIX_TAG} finished " +
                                             "with abortion verdict: ${dsbc.dsbcResultInterim}",
                                             'FAILURE', "slowbuild-run/${MATRIX_TAG}",
-                                            dsbc.getLatestDsbcResultLog())
+                                            buildArtifactUrlPrefixSlashed + dsbc.getLatestDsbcResultLog())
 
                                     parstageCompleted = true
                                     break
@@ -2788,7 +2789,7 @@ def parallelStages = prepareDynamatrix(
                                             "'slow build' stage for ${MATRIX_TAG} finished " +
                                             "with unclassified verdict: ${dsbc.dsbcResultInterim}",
                                             'FAILURE', "slowbuild-run/${MATRIX_TAG}",
-                                            dsbc.getLatestDsbcResultLog())
+                                            buildArtifactUrlPrefixSlashed + dsbc.getLatestDsbcResultLog())
 
                                     // DO NOT continue to loop
                                     parstageCompleted = true
