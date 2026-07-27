d34b953f2710f69bc33bbbac4a8887d77393281e
Jim Klimov <jimklimov+nut@gmail.com>
2026-07-27 09:27:10 +0200

    DynamatrixStash: checkoutCleanSrc(): use git metadata to try looking up REFREPO
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- src/org/nut/dynamatrix/DynamatrixStash.groovy
+++ src/org/nut/dynamatrix/DynamatrixStash.groovy
@@ -575,7 +575,13 @@ git log -1 master && exit
 
 REFREPO='${getGitRefrepoDir(script)}'
 if [ x"\${REFREPO}" = xnull ] || [ x"\${REFREPO}" = x ] || [ ! -d "\${REFREPO}" ] ; then
+    # It may end with literal '.../${GIT_SUBMODULES}' interpreted by Jenkins git-client-plugin
     REFREPO=""
+    # See if we have a record in metadata
+    if [ -s '.git/objects/info/alternates' ] ; then
+        # Can there be several entries here?
+        REFREPO="`cat .git/objects/info/alternates`"
+    fi
 fi
 
 for R in \$REFREPO `git remote` ; do
